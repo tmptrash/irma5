ab9191e7175f2560f2e86fa002ed37dad87c546a
flatline <deadbrainman@gmail.com>
2023-11-19 23:32:04 +0200

    new tests

--- src/atom.spec.js
+++ src/atom.spec.js
@@ -1,8 +1,12 @@
 import { type } from './atom.js'
 
-describe("atom module tests", function() { 
-    it("type()", function() {
-      expect(type(0b0000000000000000)).toBe(0)
-      expect(type(0b0010000000000000)).toBe(1)
-    })
+describe('atom module tests', function() { 
+  it('type()', function() {
+    expect(type(0b0000000000000000)).toBe(0b000)
+    expect(type(0b0010000000000000)).toBe(0b001)
+    expect(type(0b0100000000000000)).toBe(0b010)
+    expect(type(0b1110000000000000)).toBe(0b111)
+    expect(type(0b0001111111111111)).toBe(0b000)
+    expect(type(0b0101111111111111)).toBe(0b010)
   })
+})
