fe2a4be0dbc0193f198360cdae4097f724c0ba91
flatline <deadbrainman@gmail.com>
2023-11-19 23:42:47 +0200

    typo fix

--- spec/vm.spec.js
+++ spec/vm.spec.js
@@ -2,7 +2,7 @@ import { ATOM_TYPE_SHIFT } from '../src/shared.js'
 import { CMDS } from '../src/vm.js'
 
 describe('vm module tests', function() { 
-  it('type()', function() {
+  it('CMDS', function() {
     // amount of commands should be <= 2**3, because we use 3 bits for type
     expect(CMDS.length <= 2 ** (16 - ATOM_TYPE_SHIFT)).toBe(true)
   })
