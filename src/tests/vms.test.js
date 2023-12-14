b6758b38cd7a498e68f6cb5e0f136c003005bb78
flatline <deadbrainman@gmail.com>
2023-12-14 11:50:40 +0200

    new tests

--- src/tests/vms.test.js
+++ src/tests/vms.test.js
@@ -258,5 +258,27 @@ describe('vm module tests', () => {
       expect(get(w, WIDTH)).toBe(spl(NO_DIR, 2, 0))
       expect(vmsOffs[0] === vm(WIDTH, 1)).toBe(true)
     })
+    test('con atom should direct VM to else dir if near atom is not exist', () => {
+      const offs = 0
+      vmsOffs[0] = vm(offs, 1)
+      put(w, offs, con(0, 4, 2, NO_DIR))
+      put(w, offs + 1, mov(NO_DIR, 2))
+      put(w, WIDTH, spl(NO_DIR, 2, 0))
+      CMDS[4](vms, get(w, offs), 0)
+      expect(get(w, offs)).toBe(con(0, 4, 2, NO_DIR))
+      expect(get(w, offs + 1)).toBe(mov(NO_DIR, 2))
+      expect(get(w, WIDTH)).toBe(spl(NO_DIR, 2, 0))
+      expect(vmsOffs[0] === vm(offs + 1, 1)).toBe(true)
+    })
+    test('con atom should not direct VM to else dir if else atom is not exist', () => {
+      const offs = 0
+      vmsOffs[0] = vm(offs, 1)
+      put(w, offs, con(2, 4, 2, NO_DIR))
+      put(w, WIDTH, spl(NO_DIR, 2, 0))
+      CMDS[4](vms, get(w, offs), 0)
+      expect(get(w, offs)).toBe(con(2, 4, 2, NO_DIR))
+      expect(get(w, WIDTH)).toBe(spl(NO_DIR, 2, 0))
+      expect(vmsOffs[0] === vm(offs, 1)).toBe(true)
+    })
   })
 })
