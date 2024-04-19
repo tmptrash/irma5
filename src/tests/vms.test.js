1a320074feb21d7d672548009ba2120eb34a2419
flatline <deadbrainman@gmail.com>
2024-04-19 12:31:06 +0300

    fixed test

--- src/tests/vms.test.js
+++ src/tests/vms.test.js
@@ -190,6 +190,18 @@ describe('vm module tests', () => {
       expect(get(w, 0)).toBe(mov(NO_DIR, 0))
       expect(checkVm(vms, offs, vmIdx, energy - CFG.ATOM.NRG.fix)).toBe(false)
     })
+    test('fix atom should fix near atoms together, but should not if they already joined', () => {
+      const offs = WIDTH
+      const energy = 6 * CFG.ATOM.NRG.fix
+      const vmIdx = addVm(vms, offs, energy)
+      put(w, offs, fix(4, 2, 7))
+      put(w, 0, mov(NO_DIR, 0))
+      put(w, offs + 1, mov(2, 2))
+      CMDS[2](vms, get(w, offs), vmIdx)
+      expect(checkVm(vms, offs, vmIdx, energy - CFG.ATOM.NRG.onFix)).toBe(true)
+      CMDS[2](vms, get(w, offs), vmIdx)
+      expect(checkVm(vms, offs, vmIdx, energy - CFG.ATOM.NRG.onFix)).toBe(true)
+    })
     test('fix atom should fix near atoms together if they have only 1 bond', () => {
       const offs = WIDTH
       const energy = 6 * CFG.ATOM.NRG.fix
@@ -216,14 +228,16 @@ describe('vm module tests', () => {
       expect(get(w, 0)).toBe(mov(0, 0))
       expect(checkVm(vms, offs, vmIdx, energy)).toBe(true)
     })
-    xtest('fix atom should fix itself and near atom', () => {
+    test('fix atom should fix itself and near atom', () => {
       const offs = 0
-      vmsOffs[0] = vm(offs, 1)
+      const energy = 5 * CFG.ATOM.NRG.fix
+      const vmIdx = addVm(vms, offs, energy)
       put(w, offs, fix(NO_DIR, 2, 6))
       put(w, offs + 1, mov(0, 0))
-      CMDS[2](vms, get(w, offs), 0)
+      CMDS[2](vms, get(w, offs), vmIdx)
       expect(get(w, offs)).toBe(fix(2, 2, 6))
       expect(get(w, offs + 1)).toBe(mov(0, 0))
+      expect(checkVm(vms, offs, vmIdx, energy - CFG.ATOM.NRG.onFix)).toBe(true)
     })
     xtest('fix atom should not work if second atom is not exist', () => {
       const offs = 0
