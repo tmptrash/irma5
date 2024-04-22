b38a94f2249186312e444b62f35f5d3e5620464e
flatline <deadbrainman@gmail.com>
2024-04-22 16:03:27 +0300

    fixed two tests

--- src/tests/vms.test.js
+++ src/tests/vms.test.js
@@ -383,25 +383,27 @@ describe('vm module tests', () => {
       expect(get(w, WIDTH)).toBe(spl(NO_DIR, 2, 0))
       expect(checkVm(vms, offs + 1, vmIdx, energy - CFG.ATOM.NRG.con)).toBe(true)
     })
-    xtest('con atom should not direct VM to else dir if else atom is not exist', () => {
+    test('con atom should not direct VM to else dir if else atom is not exist', () => {
       const offs = 0
-      vmsOffs[0] = vm(offs, 1)
+      const energy = 10
+      const vmIdx = addVm(vms, offs, energy)
       put(w, offs, con(2, 4, 2, NO_DIR))
       put(w, WIDTH, spl(NO_DIR, 2, 0))
-      CMDS[4](vms, get(w, offs), 0)
+      CMDS[4](vms, get(w, offs), vmIdx)
       expect(get(w, offs)).toBe(con(2, 4, 2, NO_DIR))
       expect(get(w, WIDTH)).toBe(spl(NO_DIR, 2, 0))
-      expect(vmsOffs[0] === vm(offs, 1)).toBe(true)
+      expect(checkVm(vms, offs, vmIdx, energy)).toBe(true)
     })
-    xtest('con atom should direct VM to then atom if then and else dirs are the same', () => {
+    test('con atom should direct VM to then atom if then and else dirs are the same', () => {
       const offs = 0
-      vmsOffs[0] = vm(offs, 1)
+      const energy = 10
+      const vmIdx = addVm(vms, offs, energy)
       put(w, offs, con(2, 2, 2, NO_DIR))
       put(w, offs + 1, spl(NO_DIR, 2, 0))
-      CMDS[4](vms, get(w, offs), 0)
+      CMDS[4](vms, get(w, offs), vmIdx)
       expect(get(w, offs)).toBe(con(2, 2, 2, NO_DIR))
       expect(get(w, offs + 1)).toBe(spl(NO_DIR, 2, 0))
-      expect(vmsOffs[0] === vm(offs + 1, 1)).toBe(true)
+      expect(checkVm(vms, offs + 1, vmIdx, energy - CFG.ATOM.NRG.con)).toBe(true)
     })
     xtest('con atom should not direct VM to then atom if then atom does not exist', () => {
       const offs = 0
