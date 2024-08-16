991c83d00b2d99304ddb13a9e1b6e02a77ec5d1a
flatline <deadbrainman@gmail.com>
2024-08-16 20:22:23 +0300

    test

--- types/atom.d.ts
+++ types/atom.d.ts
@@ -1,21 +1,23 @@
-declare function type(a: number): number;
-declare function b1Dir(a: number): number;
-declare function b2Dir(a: number): number;
-declare function b3Dir(a: number): number;
-declare function ifDir(a: number): number;
-declare function thenDir(a: number): number;
-declare function setThenDir(a: number, d: number): number;
-declare function elseDir(a: number): number;
-declare function setElseDir(a: number, d: number): number;
-declare function vmDir(a: number): number;
-declare function setVmDir(a: number, d: number): number;
-/**
- * Returns 32bit offset for direction. The world is cyclical
- */
-declare function offs(offs: number, dir: number): number;
-/**
- * Returns 32bit offset obtained from vmsOffs 64 bit array
- * @param offs 64bit Offset
- * @returns 32bit offset
- */
-declare function toOffs(offs: number): number;
+declare module 'Atom' {
+  export function type(a: number): number;
+  export function b1Dir(a: number): number;
+  export function b2Dir(a: number): number;
+  export function b3Dir(a: number): number;
+  export function ifDir(a: number): number;
+  export function thenDir(a: number): number;
+  export function setThenDir(a: number, d: number): number;
+  export function elseDir(a: number): number;
+  export function setElseDir(a: number, d: number): number;
+  export function vmDir(a: number): number;
+  export function setVmDir(a: number, d: number): number;
+  /**
+   * Returns 32bit offset for direction. The world is cyclical
+   */
+  export function offs(offs: number, dir: number): number;
+  /**
+   * Returns 32bit offset obtained from vmsOffs 64 bit array
+   * @param offs 64bit Offset
+   * @returns 32bit offset
+   */
+  export function toOffs(offs: number): number;
+}
