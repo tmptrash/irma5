620c04ee2ace2120ab653683b9e0a4e38d51c958
flatline <deadbrainman@gmail.com>
2024-08-16 19:59:56 +0300

    added module declaration file for atom.js

--- src/types/atom.d.ts
+++ src/types/atom.d.ts
@@ -1,21 +1,23 @@
-export function type(a: any): number;
-export function b1Dir(a: any): number;
-export function b2Dir(a: any): number;
-export function b3Dir(a: any): number;
-export function ifDir(a: any): number;
-export function thenDir(a: any): number;
-export function setThenDir(a: any, d: any): number;
-export function elseDir(a: any): number;
-export function setElseDir(a: any, d: any): number;
-export function vmDir(a: any): number;
-export function setVmDir(a: any, d: any): number;
-/**
- * Returns 32bit offset for direction. The world is cyclical
- */
-export function offs(offs: any, dir: any): any;
-/**
- * Returns 32bit offset obtained from vmsOffs 64 bit array
- * @param offs 64bit Offset
- * @returns 32bit offset
- */
-export function toOffs(offs: any): number;
+declare module 'MAtom' {
+  export function type(a: number): number;
+  export function b1Dir(a: number): number;
+  export function b2Dir(a: number): number;
+  export function b3Dir(a: number): number;
+  export function ifDir(a: number): number;
+  export function thenDir(a: number): number;
+  export function setThenDir(a: number, d: number): number;
+  export function elseDir(a: number): number;
+  export function setElseDir(a: number, d: number): number;
+  export function vmDir(a: number): number;
+  export function setVmDir(a: number, d: number): number;
+  /**
+   * Returns 32bit offset for direction. The world is cyclical
+   */
+  export function offs(offs: number, dir: number): number;
+  /**
+   * Returns 32bit offset obtained from vmsOffs 64 bit array
+   * @param offs 64bit Offset
+   * @returns 32bit offset
+   */
+  export function toOffs(offs: number): number;
+}
