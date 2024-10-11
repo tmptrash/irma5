a020ff718c114edd241ff89840df7939d35da400
flatline <deadbrainman@gmail.com>
2024-10-11 22:08:56 +0300

    added World type

--- types/world.d.ts
+++ types/world.d.ts
@@ -1,10 +1,23 @@
 declare module 'World' {
-  export default function World(w?: number, h?: number): number;
-  export function destroy(w: World): void;
+  export type WorldType = {
+    canvas: any
+    ctx: any
+    imgData: ImageData
+    data: Uint8ClampedArray
+    zoom: any
+    zoomObserver: MutationObserver
+    animateFn: (w: WorldType) => void
+    x: number
+    y: number
+    w: number
+    h: number
+  }
+  export default function World(width?: number, height?: number): WorldType;
+  export function destroy(w: WorldType): void;
   /**
    * Returns atom or 0, if no atom
    */
-  export function get(w: World, offs: number): number;
-  export function put(w: World, offs: number, atom: number): void;
-  export function move(w: World, offs1: number, offs2: number): void;
+  export function get(w: WorldType, offs: number): number;
+  export function put(w: WorldType, offs: number, atom: number): void;
+  export function move(w: WorldType, offs1: number, offs2: number): void;
 }
