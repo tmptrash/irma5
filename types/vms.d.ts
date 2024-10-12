12ee63a83b35719438f954966a2451d20f304d68
flatline <deadbrainman@gmail.com>
2024-10-12 14:56:29 +0300

    added a type for vms

---
+++ types/vms.d.ts
@@ -0,0 +1,10 @@
+import { WorldType } from 'World'
+
+declare module 'VM' {
+  export type VMType = {
+    offs: BigUint64Array,
+    map: Map<number, Uint32Array>
+    w: WorldType
+  }
+  export default function VMs(w: WorldType, vmOffs: BigUint64Array): VMType
+}
