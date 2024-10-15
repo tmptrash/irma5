5379e38e62ac02c440c308f16a1e0eb341b5af8b
flatline <deadbrainman@gmail.com>
2024-10-15 23:21:52 +0300

    fixing types

--- types/vms.d.ts
+++ types/vms.d.ts
@@ -2,11 +2,11 @@
 
 declare module 'irma5/src/vms' {
   export type VMType = {
-    offs: BigUint64Array,
+    offs: UInt64Array,
     map: Map<number, UInt32Array>
     w: WorldType
   }
-  export default function VMs(w: WorldType, vmOffs: BigUint64Array): VMType;
+  export default function VMs(w: WorldType, vmOffs: UInt64Array): VMType;
   export function vm(offs: number, energy: number = 0): BigInt
   export function nrg(offs: BigInt): number
   export function tick(vms: VMType, vmIdx: number): number
