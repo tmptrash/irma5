9668beaeb81c73959d56022afbe82c6add7e162b
flatline <deadbrainman@gmail.com>
2025-06-13 16:28:38 +0300

    updated types

--- types/shared.d.ts
+++ types/shared.d.ts
@@ -1,10 +1,16 @@
 declare module 'irma5/src/shared' {
-  export const VM_OFFS_MASK: 18446744069414584320n;
+  export const VM_OFFS_MASK: 0xFFFFFFFF00000000n;
   export const VM_OFFS_SHIFT: 32n;
-  export const VM_ENERGY_MASK: 4294967295n;
-  export const MOV_BREAK_MASK: 2147483648;
-  export const MOV_BREAK_UNMASK: 2147483647;
+  export const VM_ENERGY_MASK: 0x00000000FFFFFFFFn;
+  export const MOV_BREAK_MASK: 0b10000000000000000000000000000000;
+  export const MOV_BREAK_UNMASK: 0b01111111111111111111111111111111;
+
   export const NO_DIR: -1;
+
+  export const MASK_4BITS: 0b1111;
+  export const MASK_3BITS: 0b111;
+  export const MASK_2BITS: 0b11;
+
   export const ATOM_NOP: 0;
   export const ATOM_MOV: 1;
   export const ATOM_FIX: 2;
@@ -13,6 +19,7 @@ declare module 'irma5/src/shared' {
   export const ATOM_JOB: 5;
   export const ATOM_REP: 6;
   export const ATOM_MUT: 7;
+
   export const ATOM_TYPE_MASK: 57344;
   export const ATOM_TYPE_UNMASK: 8191;
   export const ATOM_TYPE_SHIFT: 13;
@@ -22,11 +29,15 @@ declare module 'irma5/src/shared' {
   export const ATOM_BOND1_MASK: 448;
   export const ATOM_BOND1_MASK1: 65087;
   export const ATOM_BOND1_SHIFT: 6;
+  export const ATOM_SECTION_MASK: 0b0000000000110000;
+  export const ATOM_SECTION_MASK1: 0b1111111111001111;
+  export const ATOM_SECTION_SHIFT: 4;
   export const ATOM_BOND2_MASK: 56;
   export const ATOM_BOND2_MASK1: 65479;
   export const ATOM_BOND2_SHIFT: 3;
   export const ATOM_BOND3_MASK: 15;
   export const ATOM_BOND3_MASK1: 65520;
+  export const ATOM_SECTION_VAL_MASK: 0b0000000000001111;
   export const ATOM_IF_BOND_MASK: 7168;
   export const ATOM_IF_BOND_MASK1: 58367;
   export const ATOM_IF_BOND_SHIFT: 10;
@@ -79,6 +90,17 @@ declare module 'irma5/src/shared' {
    * === DNA[6][0] === 7
    */
   export const DNA: number[][];
+  /**
+   * All available directions
+   */
+  export const U:  0; // up
+  export const UR: 1; // up-right
+  export const R:  2; // right
+  export const RD: 3; // right-down
+  export const D:  4; // down
+  export const DL: 5; // ...
+  export const L:  6;
+  export const LU: 7;
   /**
    * Wrapper for Uint32Array type with an ability to create, resize, add,
    * remove elements. The difference between this version and original is 
