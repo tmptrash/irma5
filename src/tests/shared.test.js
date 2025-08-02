6f939268ec07080cbbae2b1b495b7e430e5e5bc2
Dmytro Tarasenko <dtarasen@cisco.com>
2025-08-02 17:03:31 +0300

    added a test

--- src/tests/shared.test.js
+++ src/tests/shared.test.js
@@ -1,5 +1,21 @@
 import { rnd, setRndSeed, UInt32Array } from "../shared";
 
+beforeAll(() => {
+  //
+  // This code is used for Node.js environment for the tests
+  //
+  if (!ArrayBuffer.prototype.transfer) {
+    ArrayBuffer.prototype.transfer = function transfer(len) {
+      if (!(this instanceof ArrayBuffer)) throw new TypeError('Source must be an instance of ArrayBuffer')
+      if (len <= this.byteLength) return this.slice(0, len)
+      const sourceView = new Uint8Array(this)
+      const destView = new Uint8Array(new ArrayBuffer(len))
+      destView.set(sourceView)
+      return destView.buffer
+    }
+  }
+})
+
 describe('Shared functions', () => {
   describe('rnd() function logic', () => {
     it('Resetting seed to the same value should get us the same rnd() result', () => {
@@ -34,6 +50,17 @@ describe('Shared functions', () => {
       expect(a[0]).toBe(42)
       expect(a[1]).toBe(43)
       expect(a.end()).toBe(true)
+    })
+    it('resize() should double the size of the array', () => {
+      let a = UInt32Array.create(1)
+      a.add(42)
+      expect(a.end()).toBe(true)
+      a = a.resize(2)
+      expect(a.end()).toBe(false)
+      a.add(44)
+      expect(a[1]).toBe(44)
+      expect(a.end()).toBe(true)
+      expect(a.i).toBe(2)
     })
   })
 });
