4d1be9c5b42ebda26774696da61fb513dc46bffc
flatline <deadbrainman@gmail.com>
2025-08-05 23:57:55 +0300

    fixed naming

--- src/tests/vms.test.js
+++ src/tests/vms.test.js
@@ -4,7 +4,7 @@ import { ATOM_TYPE_SHIFT, NO_DIR, UInt64Array, R, L, U, UR, D, DL, LU, RD, ATOM_
 import VMs, { CMDS, addVm } from '../vms'
 import World, { destroy, get } from '../world'
 import { mov, fix, spl, con, job, rep, mut } from './../atom'
-import { testAtoms } from './utils.test'
+import { testAtoms } from './utils'
 
 // TODO: add mov-fix atoms moving horizontally and fix two or more atoms above
 // TODO: add complex atoms tests. like more than 3,5,...
